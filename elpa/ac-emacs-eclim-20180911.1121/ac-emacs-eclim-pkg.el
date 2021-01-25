3a811a75b2af755567105b71fa0643f40498d890
catLee <catlee@plurk.com>
2021-01-25 11:18:06 +0800

    add back elpa packages

---
+++ elpa/ac-emacs-eclim-20180911.1121/ac-emacs-eclim-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ac-emacs-eclim" "20180911.1121" "auto-complete source for eclim" '((eclim "0.3") (auto-complete "1.5")) :commit "222ddd48fcf0ee01592dec77c58e0cf3f2ea1100")
---
+++ elpa/java-snippets-20160627.252/java-snippets-pkg.el
@@ -0,0 +1,10 @@
+(define-package "java-snippets" "20160627.252" "Yasnippets for Java"
+  '((yasnippet "0.8.0"))
+  :commit "6d0e2768823be27dbe07448f4cb244cd657a7136" :authors
+  '(("Takayoshi Kimura"))
+  :maintainer
+  '("Takayoshi Kimura")
+  :url "https://github.com/nekop/yasnippet-java-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
