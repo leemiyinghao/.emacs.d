ea67114b289716a05f1eb2a032bfd27be4268c20
catLee <leemiyinghao@gmx.com>
2021-02-06 15:22:57 +0800

    add support for rust

---
+++ elpa/elpa/elpl-20200821.1052/elpl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elpl" "20200821.1052" "Emacs Lisp REPL" '((emacs "24.4")) :commit "ca6a6237681c641d5137d58e52f884dec0da6349" :authors '(("Gong Qijian" . "gongqijian@gmail.com")) :maintainer '("Gong Qijian" . "gongqijian@gmail.com") :keywords '("lisp" "tool") :url "https://github.com/twlz0ne/elpl")
