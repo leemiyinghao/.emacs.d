29962a573b80ea1b964847c7424b30c7a8e7d1ea
catLee <leemiyinghao@gmx.com>
2023-02-17 20:40:05 +0800

    Update font-size, line-spacing

--- gui.el
+++ gui.el
@@ -1,6 +1,8 @@
 (toggle-scroll-bar nil)
 (add-to-list 'default-frame-alist
-             '(font . "Fira Code 16"))
+             '(font . "Fira Code 14"))
 
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
+(setq-default line-spacing 2)
+
