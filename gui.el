0217e727d1893d3b9d66a60c6eac5ac95d9cd1bf
catLee <leemiyinghao@gmx.com>
2023-02-18 00:06:51 +0800

    Add transparent

--- gui.el
+++ gui.el
@@ -12,7 +12,15 @@
 '(mode-line-emphasis ((t (:weight bold))))
 '(mode-line-highlight ((((class color) (min-colors 88)) (:box (:line-width 2 :color "grey40" :style released-button))) (t (:inherit (highlight)))))
 '(mode-line-inactive ((t (:weight light :underline nil :overline nil :box (:line-width 8 :color "#565063" :style nil) :foreground "white" :background "#565063" :inherit (mode-line)))))
+
 ;; tab bar beautify
 '(tab-bar ((t (:inherit mode-line))))
 '(tab-bar-tab ((t (:inherit mode-line :foreground "white"))))
 '(tab-bar-tab-inactive ((t (:inherit mode-line-inactive :foreground "black"))))
+
+;; frame transparent
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
+
+;; maximized
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
--- ui.el
+++ ui.el
@@ -2,10 +2,3 @@
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 
-;; tab-bars
-(tab-bar-mode 1)
-(setq tab-bar-show 1)
-(setq tab-bar-close-button-show nil)
-(setq tab-bar-new-tab-choice "*dashboard*")
-(setq tab-bar-tab-hints t)
-(setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
