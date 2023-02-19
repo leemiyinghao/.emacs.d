2e38f928031023142788b395f2a97b777f93d620
leemiyinghao <leemiyinghao@ryzen-wugu.ryzen.catlee.se>
2023-02-20 01:40:19 +0800

    Add ein, not manage to properly print plotly graphs

--- configs/python.el
+++ configs/python.el
@@ -7,4 +7,13 @@
                     :server-id 'pylsp)))
 (add-hook 'python-mode 'python-hook)
 
+(use-package pipenv
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setq
+   pipenv-projectile-after-switch-function
+   #'pipenv-projectile-after-switch-extended))
+
 (use-package jupyter)
+
+(use-package ein)
--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "c4cecd97a6b30d129971302fd8298c2ff56189db0a94570e7238bc95f9389cfb" "b07a04cf42fe1001b2119b45f40cbc368783b5b8e1721ce3728a5f2823d97a66" default))
  '(flycheck-checker-error-threshold 4096)
  '(package-selected-packages
-   '(jupyter yaml-mode json-mode dockerfile-mode nginx-mode magit multiple-cursors use-package neotree kivy-mode ripgrep flycheck lsp-ui faces labburn-theme nyan-mode autopair guru-mode which-key company counsel ivy lsp-mode))
+   '(ein pipenv jupyter yaml-mode json-mode dockerfile-mode nginx-mode magit multiple-cursors use-package neotree kivy-mode ripgrep flycheck lsp-ui faces labburn-theme nyan-mode autopair guru-mode which-key company counsel ivy lsp-mode))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
