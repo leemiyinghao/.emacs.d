c1a2ed2c5a36e874880bd40f300a4dd9ae53620e
catLee <catlee@plurk.com>
2023-03-21 18:11:13 +0800

    Properly set package dependencies for magit-related

--- configs/magit.el
+++ configs/magit.el
@@ -1,4 +1,10 @@
 (use-package magit :ensure t)
-(use-package forge :defer t)
-(use-package magithub :defer t)
-(use-package magit-todos :defer t)
+(use-package forge
+  :defer t
+  :after magit)
+(use-package magithub
+  :defer t
+  :after magit)
+(use-package magit-todos
+  :defer t
+  :after magit)
