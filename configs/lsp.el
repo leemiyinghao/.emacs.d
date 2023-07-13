38c101eab02a44bd84fd9e81da071c6d9a8fc37a
catLee <leemiyinghao@gmx.com>
2023-07-14 01:01:20 +0800

    Add grammarly-languageserver for better spelling on text related modes.

--- configs/lsp.el
+++ configs/lsp.el
@@ -1,54 +1,24 @@
 "lsp"
 
-;; (setq lsp-keymap-prefix "C-c l")
-
-;; (use-package lsp-mode
-;;   :ensure t
-;;   :defer t
-;;   :config
-;;   (setq lsp-idle-delay 0.5
-;;         lsp-enable-symbol-highlighting t
-;;         lsp-enable-snippet nil
-;; 	gc-cons-threshold 100000000
-;; 	read-process-output-max (* 4096 4096)
-;; 	lsp-log-io nil)
-;;   (lsp-register-client
-;;    (make-lsp-client :new-connection (lsp-stdio-connection "pylsp")
-;;                     :activation-fn (lsp-activate-on "python")
-;;                     :server-id 'pylsp))
-;;   :hook
-;;   ((lsp-mode . (lambda ()
-;;                       (let ((lsp-keymap-prefix "C-c l"))
-;;                         (lsp-enable-which-key-integration))))
-;;   (python-mode . lsp)
-;;   (rust-mode . lsp)
-;;   ))
-;; ; (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
-
-;; (use-package lsp-ui
-;;   :ensure t
-;;   :config (setq lsp-ui-sideline-show-hover t
-;;                 lsp-ui-sideline-delay 0.5
-;; 		lsp-ui-peek-always-show t
-;; 		lsp-ui-doc-enable t
-;;                 lsp-ui-doc-delay 5
-;;                 lsp-ui-doc-position 'bottom
-;;                 lsp-ui-doc-alignment 'frame
-;;                 lsp-ui-doc-header nil
-;;                 lsp-ui-doc-include-signature t
-;;                 lsp-ui-doc-use-childframe t)
-;;   :commands lsp-ui-mode)
-
 (use-package eglot
   :defer t
   :hook ((python-mode . eglot-ensure)
 	 (rust-mode . eglot-ensure)
-	 (vue-mode-reparse . eglot-ensure))
+	 (vue-mode-reparse . eglot-ensure)
+	 (text-mode . eglot-ensure)
+	 (bibtex-mode . eglot-ensure)
+	 (context-mode . eglot-ensure)
+	 (latex-mode . eglot-ensure)
+	 (markdown-mode . eglot-ensure)
+	 (org-mode . eglot-ensure)
+	 (rst-mode . eglot-ensure))
   :bind (("C-c l = =" . eglot-format)
-	 ("C-c l r" . eglot-rename))
+	 ("C-c l r" . eglot-rename)
+	 ("C-<tab>" . eglot-code-actions))
   :config
   (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
   (add-to-list 'eglot-server-programs '(vue-mode . ("vue-language-server" "--stdio")))
+  (add-to-list 'eglot-server-programs '('(text-mode bibtex-mode context-mode latex-mode markdown-mode org-mode rst-mode) . ("grammarly-languageserver" "--stdio" :initializationOptions (:clientId "client_BaDkMgx4X19X9UxxYRCXZo"))))
   (add-to-list 'eglot-stay-out-of 'company))
 
 (use-package flymake-diagnostic-at-point
@@ -57,16 +27,3 @@
   (add-hook 'flymake-mode-hook #'flymake-diagnostic-at-point-mode)
   (setq flymake-diagnostic-at-point-display-diagnostic-function 'flymake-diagnostic-at-point-display-minibuffer))
 
-;; (use-package eldoc-overlay
-;;   :init (eldoc-overlay-mode 1)
-;;   :config
-;;   (setq eldoc-overlay-backend 'quick-peek))
-
-
-;; Grammarly for eglot
-(use-package eglot-grammarly
-  :straight (:host github :repo "emacs-grammarly/eglot-grammarly")
-  :defer t  ; defer package loading
-  :hook ((text-mode markdown-mode). (lambda ()
-                                      (require 'eglot-grammarly)
-                                      (eglot-ensure))))
