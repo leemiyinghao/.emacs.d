682f7f0632979f3b27b8188d968d71b52bb08e9b
catLee <leemiyinghao@gmx.com>
2023-08-25 17:46:18 +0800

    Add treesit-auto for auto-installation

--- configs/treesitter.el
+++ configs/treesitter.el
@@ -1,6 +1,10 @@
 (setq major-mode-remap-alist
- '((yaml-mode . yaml-ts-mode)
-   (bash-mode . bash-ts-mode)
-   (json-mode . json-ts-mode)
-   (css-mode . css-ts-mode)
-   (python-mode . python-ts-mode)))
+      '((yaml-mode . yaml-ts-mode)
+	(bash-mode . bash-ts-mode)
+	(json-mode . json-ts-mode)
+	(css-mode . css-ts-mode)
+	(python-mode . python-ts-mode)))
+
+(use-package treesit-auto
+  :config
+  (global-treesit-auto-mode))
