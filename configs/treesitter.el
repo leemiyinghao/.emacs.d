929549e4ba3fdfa3eb813f99000c112eaef353e2
catLee <leemiyinghao@gmx.com>
2023-09-16 17:52:13 +0800

    Switch back to tree-sitter.el, add ts-docstr.

--- configs/treesitter.el
+++ configs/treesitter.el
@@ -1,14 +1,14 @@
-(setq major-mode-remap-alist
-      '((yaml-mode . yaml-ts-mode)
-	(bash-mode . bash-ts-mode)
-	(json-mode . json-ts-mode)
-	(css-mode . css-ts-mode)
-	(python-mode . python-ts-mode)))
+;; (setq major-mode-remap-alist
+;;       '((yaml-mode . yaml-ts-mode)
+;; 	(bash-mode . bash-ts-mode)
+;; 	(json-mode . json-ts-mode)
+;; 	(css-mode . css-ts-mode)
+;; 	(python-mode . python-ts-mode)))
 
-(use-package treesit-auto
-  :defer t
-  :config
-  (global-treesit-auto-mode))
+;; (use-package treesit-auto
+;;   :defer t
+;;   :config
+;;   (global-treesit-auto-mode))
 
 (use-package combobulate
   :straight (:host github :repo "mickeynp/combobulate")
@@ -23,3 +23,15 @@
          (json-ts-mode . combobulate-mode)
          (typescript-ts-mode . combobulate-mode)
          (tsx-ts-mode . combobulate-mode)))
+(use-package tree-sitter
+  :config (global-tree-sitter-mode))
+(use-package tree-sitter-langs)
+
+(use-package msgu
+  :straight (:host github :repo "jcs-elpa/msgu"
+                   :files (:defaults "langs/*.el")))
+;; ts-docstr
+(use-package ts-docstr 
+  :straight (ts-docstr :type git :host github :repo "emacs-vs/ts-docstr"
+                       :files (:defaults "langs/*.el")))
+
