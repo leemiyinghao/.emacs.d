36427763524519666eb08319401a38c9e57c164a
catLee <leemiyinghao@gmx.com>
2023-09-20 21:42:25 +0800

    Adjust markdown template.

--- configs/markdown.el
+++ configs/markdown.el
@@ -13,16 +13,24 @@
         markdown-fontify-code-blocks-natively t
 
         markdown-content-type "application/xhtml+xml"
-        markdown-css-paths '("https://cdn.jsdelivr.net/npm/github-markdown-css/github-markdown.min.css"
+        markdown-css-paths '("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.2.0/github-markdown-dark.min.css"
                              "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/github.min.css")
         markdown-xhtml-header-content "
 <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 <style>
-body {
-  box-sizing: border-box;
-  margin: 40px auto;
-  padding: 0 10px;
-}
+	.markdown-body {
+		box-sizing: border-box;
+		min-width: 200px;
+		max-width: 980px;
+		margin: 0 auto;
+		padding: 45px;
+	}
+
+	@media (max-width: 767px) {
+		.markdown-body {
+			padding: 15px;
+		}
+	}
 </style>
 
 <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/styles/default.min.css'>
