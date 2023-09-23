3a33b6e5cca77537df5c3fe338c924fd27115ac7
catLee <leemiyinghao@gmx.com>
2023-09-23 21:14:38 +0800

    Add dimmer.el.

--- ui.el
+++ ui.el
@@ -23,5 +23,16 @@
   (setq dashboard-center-content t)
   (setq dashboard-icon-type 'all-the-icons)
   (setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5))))
+                          (bookmarks . 5)
+                          (projects . 5))))
+
+(use-package dimmer
+  :straight (:host github :repo "gonewest818/dimmer.el")
+  :config
+  (setq dimmer-adjustment-mode :foreground)
+  (setq dimmer-fraction 0.61)
+  (dimmer-configure-which-key)
+  (dimmer-configure-magit)
+  (dimmer-configure-posframe)
+  (dimmer-mode t))
+
