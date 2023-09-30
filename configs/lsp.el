9eb1a23d5cafe7db09a2b60603821eec7e3185b1
catLee <leemiyinghao@gmx.com>
2023-10-01 00:15:56 +0800

    Add typescript-language-server for javascript-mode.

--- configs/lsp.el
+++ configs/lsp.el
@@ -7,6 +7,7 @@
 	 (rust-mode . eglot-ensure)
 	 (vue-mode-reparse . eglot-ensure)
 	 (text-mode . eglot-ensure)
+	 (javascript-mode . eglot-ensure)
 	 (bibtex-mode . eglot-ensure)
 	 (context-mode . eglot-ensure)
 	 (latex-mode . eglot-ensure)
@@ -20,6 +21,7 @@
   (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
   (add-to-list 'eglot-server-programs '(python-ts-mode . ("pyright-langserver" "--stdio")))
   (add-to-list 'eglot-server-programs '(vue-mode . ("vue-language-server" "--stdio")))
+  (add-to-list 'eglot-server-programs '(javascript-mode . ("typescript-language-server" "--stdio")))
   (add-to-list 'eglot-server-programs '('(text-mode bibtex-mode context-mode latex-mode markdown-mode org-mode rst-mode) . ("grammarly-languageserver" "--stdio" :initializationOptions (:clientId "client_BaDkMgx4X19X9UxxYRCXZo"))))
   (add-to-list 'eglot-stay-out-of 'company))
 
