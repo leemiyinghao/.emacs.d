c094bd2a5577283452490901b0f7989d000982ee
leemiyinghao <leemiyinghao@gmx.com>
2024-05-12 13:19:36 +0800

    Update copilot keybind, add git-commit-mode to copilot.

--- configs/config-copilot.el
+++ configs/config-copilot.el
@@ -3,7 +3,9 @@
 (use-package copilot
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
   :hook (prog-mode . copilot-mode)
-  :bind (("M-<return>" . copilot-accept-completion))
+        (git-commit-mode . copilot-mode)
+  :bind (("M-RET" . copilot-accept-completion)
+	 ("M-<return>" . copilot-accpet-completion))
   ;; suppress warning
   :config (setq copilot-indent-offset-warning-disable t))
 
