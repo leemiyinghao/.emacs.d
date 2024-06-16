74545e14ad1d67ef236d9eb5cb2c6d4d1a4a0add
catLee <leemiyinghao@gmx.com>
2024-06-16 17:36:12 +0800

    Bind copilot to more modes.

--- configs/config-copilot.el
+++ configs/config-copilot.el
@@ -2,11 +2,13 @@
 
 (use-package copilot
   :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
-  :hook (prog-mode . copilot-mode)
-        (git-commit-mode . copilot-mode)
-        (org-mode . copilot-mode)
+  :hook ((prog-mode . copilot-mode)
+         (git-commit-mode . copilot-mode)
+         (org-mode . copilot-mode)
+		 (yaml-mode . copilot-mode))
   :bind (("M-RET" . copilot-accept-completion)
-	 ("M-<return>" . copilot-accpet-completion))
+	 (:map org-mode-map
+	       ("M-RET" . copilot-accept-completion)))
   ;; suppress warning
   :config (setq copilot-indent-offset-warning-disable t))
 
