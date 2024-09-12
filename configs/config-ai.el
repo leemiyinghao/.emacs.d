673fbf49c8f5ef4bebf3542484b2fb126c31b58e
catLee <leemiyinghao@gmx.com>
2024-09-12 19:05:16 +0800

    refactor: Added magit-gptcommit integration to the configuration file

--- configs/config-ai.el
+++ configs/config-ai.el
@@ -6,5 +6,14 @@
 (use-package elysium
   :ensure (:host github :repo "lanceberge/elysium"))
 
+(use-package magit-gptcommit
+  :demand t
+  :after magit
+  :bind (:map git-commit-mode-map
+              ("C-c C-g" . magit-gptcommit-commit-accept))
+  :config
+  (magit-gptcommit-status-buffer-setup))
+
+
 (provide 'config-ai)
 ;;; config-ai.el ends here
