886d2082b34e2e2e4e898685ac13ad21219b4b4c
catLee <leemiyinghao@gmx.com>
2025-03-02 12:35:36 +0800

    feat: Add electric pair configurations for web modes

--- configs/config-web.el
+++ configs/config-web.el
@@ -5,6 +5,26 @@
 
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
+
+(defun config-html--setup ()
+  "Setup web development configuration for HTML."
+  (setq-local electric-pair-pairs
+			  (append electric-pair-pairs '((?< . ?>)))))
+
+(defun config-javascript--setup ()
+  "Setup web development configuration for JavaScript."
+  (setq-local electric-pair-pairs
+			  (append electric-pair-pairs '((?` . ?`)
+											(?' . ?')
+											(?{ . ?})
+											(?[ . ?])))))
+
+(add-hook 'tsx-ts-mode-hook #'config-html--setup)
+(add-hook 'tsx-ts-mode-hook #'config-javascript--setup)
+(add-hook 'typescript-ts-mode-hook #'config-javascript--setup)
+(add-hook 'javascript-ts-mode-hook #'config-javascript--setup)
+(add-hook 'html-mode-hook #'config-html--setup)
+
 (provide 'config-web)
 
 ;;; config-web.el ends here
