0dfadb37f719f5c150a51f3db5c186633baf57ca
catLee <leemiyinghao@gmx.com>
2025-03-06 20:55:29 +0800

    feat: Add flymake-eslint configuration for web modes

--- configs/config-web.el
+++ configs/config-web.el
@@ -25,6 +25,23 @@
 (add-hook 'javascript-ts-mode-hook #'config-javascript--setup)
 (add-hook 'html-mode-hook #'config-html--setup)
 
+(defun flymake-eslint-enable-current-project ()
+  "Enable flymake-eslint with current project eslint binary, if available."
+  (interactive)
+
+  (let* ((pj-root (project-root (project-current)))
+		 (looking-for-binary (concat pj-root "node_modules/.bin/eslint")))
+	(when (and pj-root (file-exists-p looking-for-binary)))
+	(setq-local flymake-eslint-project-root pj-root)
+	(setq-local flymake-eslint-executable-name looking-for-binary))
+  (flymake-eslint-enable))
+
+(use-package flymake-eslint
+  :ensure t
+  :config
+  (add-hook 'typescript-ts-mode-hook #'flymake-eslint-enable-current-project)
+  (add-hook 'tsx-ts-mode-hook #'flymake-eslint-enable-current-project))
+
 (provide 'config-web)
 
 ;;; config-web.el ends here
