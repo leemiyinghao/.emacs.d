1d972c7b3ce6fbe04c998341ad21726d4be93d8b
leemiyinghao <leemiyinghao@gmx.com>
2026-05-30 14:43:39 +0800

    fix: Fix remote keybindings in terminal

--- configs/config-qol.el
+++ configs/config-qol.el
@@ -16,6 +16,12 @@
 (setq-default tab-width 4)
 (setq tab-width 4)
 
+;; fix remote keybinding in terminal
+(define-key input-decode-map "\e[1;2D" [S-left])
+(define-key input-decode-map "\e[1;2C" [S-right])
+(define-key input-decode-map "\e[1;2A" [S-up])
+(define-key input-decode-map "\e[1;2B" [S-down])
+
 ;; enable auto revert mode
 (global-auto-revert-mode t)
 
